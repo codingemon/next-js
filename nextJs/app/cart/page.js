733954dddbf9fd44749820c6ac77c0bc83caf49d
snoopy1224 <misty12024@gmail.com>
2023-07-26 19:42:16 +0900

    nextJs

--- nextJs/app/cart/page.js
+++ nextJs/app/cart/page.js
@@ -1,7 +1,17 @@
 export default function Cart() {
   return (
     <div>
-      <h2>Cart페이지입니다.</h2>
+      <h4 className="title">Cart</h4>
+      <div className="cart-item">
+        <p>상품명</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
+      <div className="cart-item">
+        <p>상품명</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
     </div>
   );
 }
--- nextJs/app/list/page.js
+++ nextJs/app/list/page.js
@@ -6,8 +6,9 @@ export default function List() {
       <h4 className="title">상품목록</h4>
       {상품.map((a, i) => {
         return (
-          <div className="food">
-            <h4>{상품[i]} $40</h4>
+          <div className="food" key={i}>
+            <img src={`/food${i}.png`} className="food-img" />
+            <h4>{a} $40</h4>
           </div>
         );
       })}
