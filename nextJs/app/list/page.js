9184236bdb8aba89f0595566aac491272d9f176c
snoopy1224 <misty12024@gmail.com>
2023-07-27 23:37:22 +0900

    nextJs

--- nextJs/app/list/page.js
+++ nextJs/app/list/page.js
@@ -1,5 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
 export default function List() {
   let 상품 = ["Tomatoes", "Pasta", "Coconut"];
+  let [수량, 수량변경] = useState(0);
 
   return (
     <div>
@@ -9,6 +14,22 @@ export default function List() {
           <div className="food" key={i}>
             <img src={`/food${i}.png`} className="food-img" />
             <h4>{a} $40</h4>
+            <span> {수량} </span>
+            <button
+              onClick={() => {
+                수량변경(수량 + 1);
+              }}
+            >
+              +
+            </button>
+            {/* 숙제 */}
+            <button
+              onClick={() => {
+                수량변경(수량 - 1);
+              }}
+            >
+              -
+            </button>
           </div>
         );
       })}
