e30040e548ef270d4bad476a07a91043120a790e
snoopy1224 <misty12024@gmail.com>
2023-07-27 22:02:19 +0900

    props

--- nextJs/app/cart/page.js
+++ nextJs/app/cart/page.js
@@ -1,38 +1,35 @@
 import 작명 from "./data.js";
-import 작명2 from "../assignment/hello.js";
-
-// 2. client component (파일 맨위에 'use client' 넣고 만든거)
-// html에 자바스크립트 기능넣기 가능, useState, useEffect등 사용가능
-// 로딩속도 느림(자바스크립트 많이 필요), 로딩속도 느림2(hydration 필요)
 
 export default function Cart() {
+  let 장바구니 = ["Tomatoes", "Pasta"];
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <CartItem />
-      <CartItem />
-      <CartItem />
-      <div>{작명2}</div>
+      <CartItem item={장바구니[0]} />
+      <CartItem item={장바구니[1]} />
+      <Banner content="롯데카드" />
+      <Banner content="현대카드" />
+      <Button color="blue" />
     </div>
   );
 }
 
-// component 만들기
-// 1. function 작명(){}
-// 2. return(축약할 긴 HTML)
-// 3. <작명/> 사용
-// Q. 눈에 보이는거 전부 다 component로 만들면 좋을까? -> component끼리 데이터 공유 귀찮아짐 (복잡해짐)
-// 재사용이 잦은 html 덩어리들을 주로 component로 만드는게 좋음
-// Next.js 컴포넌트 종류가 2개
-// 1. server component 2. client component
-// 큰페이지 server component, js기능 필요한 곳만 client component 추천
+function Button(props) {
+  return (
+    <div>
+      <button style={{ background: props.color }}>버튼</button>
+    </div>
+  );
+}
 
-// 1. server component (아무데나 대충 만든거)
-// html 자바스크립트 기능넣기 불가능, 로딩속도 빠름, 검색엔진 노출
-function CartItem() {
+function Banner(props) {
+  return <h5>{props.content} 결제 행사중</h5>;
+}
+
+function CartItem(props) {
   return (
     <div className="cart-item">
-      <p>상품명</p>
+      <p>{props.item}</p>
       <p>$40</p>
       <p>1개</p>
     </div>
