a21e859be5959a9d4e1d468b1cb1402b5dc83270
snoopy1224 <misty12024@gmail.com>
2023-07-29 23:40:44 +0900

    db

---
+++ forum/app/list/page.js
@@ -0,0 +1,21 @@
+import { connectDB } from "@/util/database";
+
+export default async function List() {
+  const db = (await connectDB).db("forum");
+  let result = await db.collection("post").find().toArray();
+
+  console.log(result[0].title);
+
+  return (
+    <div className="list-bg">
+      {result.map((a, i) => {
+        return (
+          <div className="list-item">
+            <h4>{result[i].title}</h4>
+            <p>1월 1일</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
