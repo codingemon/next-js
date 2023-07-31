7956adbc419e2ce7dad449f56a6449e9a2bd98ea
snoopy1224 <misty12024@gmail.com>
2023-07-31 19:01:43 +0900

    next

--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -1,22 +1,22 @@
 import { connectDB } from "@/util/database";
 import Link from "next/link";
+import DetailLink from "./DetailLink";
 
 export default async function List() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
 
-  console.log(result[0].title);
-
   return (
     <div className="list-bg">
       {result.map((a, i) => {
         return (
-          <Link href={"/detail/" + result[i]._id}>
-            <div className="list-item">
+          <div className="list-item">
+            <Link prefetch={false} href={"/detail/" + result[i]._id}>
               <h4>{result[i].title}</h4>
               <p>1월 1일</p>
-            </div>
-          </Link>
+            </Link>
+            <DetailLink />
+          </div>
         );
       })}
     </div>
