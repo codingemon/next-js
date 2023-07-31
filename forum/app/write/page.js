0203188ca32e11cd1c1c308805d1ca88bf7b07f2
snoopy1224 <misty12024@gmail.com>
2023-08-01 02:23:18 +0900

    수정

--- forum/app/write/page.js
+++ forum/app/write/page.js
@@ -5,7 +5,7 @@ export default function Write() {
       <form action="/api/post/new" method="POST">
         <input name="title" placeholder="글제목" />
         <input name="content" placeholder="글내용" />
-        <button type="sumbit">버튼</button>
+        <button type="sumbit">작성완료</button>
       </form>
     </div>
   );
