990c744a61c710a4d6c64a3ead59bcd03913ca5a
snoopy1224 <misty12024@gmail.com>
2023-08-02 22:42:52 +0900

    next

--- forum/pages/api/post/new.js
+++ forum/pages/api/post/new.js
@@ -6,7 +6,7 @@ export default async function handler(요청, 응답) {
     if (요청.body.title == "") {
       return 응답.status(500).json("너 제목 왜 안씀?");
     } else if (요청.body.content == "") {
-      return 응답.status(500).json("넌 글내용 왜 안씀?");
+      return 응답.status(500).json("넌 글내용 왜 안씀???");
     }
     const db = (await connectDB).db("forum");
     let result = await db.collection("post").insertOne(요청.body);
