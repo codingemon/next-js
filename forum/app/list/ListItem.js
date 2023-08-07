8134b84555b01f8da1fe983f0dded6388f0a4bfa
snoopy1224 <misty12024@gmail.com>
2023-08-07 13:02:08 +0900

    next

--- forum/app/list/ListItem.js
+++ forum/app/list/ListItem.js
@@ -11,6 +11,30 @@ export default function ListItem({ result }) {
             <h4>{result[i].title}</h4>
           </Link>
           <Link href={"/edit/" + result[i]._id}>수정</Link>
+          <span
+            onClick={() => {
+              fetch("/api/post/delete", {
+                method: "DELETE",
+                body: result[i]._id,
+              })
+                .then((r) => {
+                  if (r.status == 200) {
+                    return r.json();
+                  } else {
+                    //서버가 에러코드전송시 실행할 코드
+                  }
+                })
+                .then((result) => {
+                  //성공시 실행할 코드
+                })
+                .catch((error) => {
+                  //인터넷문제로 실패시 실행할 코드
+                  console.log(error);
+                });
+            }}
+          >
+            / 삭제
+          </span>
           <p>1월 1일</p>
         </div>
       ))}
