f7e7754f65b2068e65da2b27d1604010fbc9197b
snoopy1224 <misty12024@gmail.com>
2023-08-11 23:42:24 +0900

    댓글 기능

---
+++ forum/app/detail/[id]/Comment.js
@@ -0,0 +1,24 @@
+"use client";
+import { useState } from "react";
+
+export default function Comment(props) {
+  let [comment, setComment] = useState("");
+  return (
+    <div>
+      <div>댓글목록</div>
+      <input
+        onChange={(e) => {
+          setComment(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          console.log(comment);
+          fetch("/URL", { method: "POST", body: comment });
+        }}
+      >
+        댓글전송
+      </button>
+    </div>
+  );
+}
