0c41a09a7ef37ff3b31c8fb4442d40909c2f24c6
snoopy1224 <misty12024@gmail.com>
2023-08-12 13:05:46 +0900

    댓글기능

--- forum/app/detail/[id]/Comment.js
+++ forum/app/detail/[id]/Comment.js
@@ -1,9 +1,18 @@
 "use client";
 // 서버에게 DB출력결과 달라고 부탁은 가능
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Comment(props) {
   let [comment, setComment] = useState("");
+
+  // 쓸데없는 코드 보관함 (ajax, 타이머등 넣어줌)
+  // useEffect 특징
+  // 1. html 로드/재렌더링될 때 마다 실행됨
+  // 2. html 보여준 후에 늦게 실행시작
+  useEffect(() => {
+    fetch();
+  }, []);
+
   return (
     <div>
       <div>댓글목록</div>
