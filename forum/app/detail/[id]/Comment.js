b16feb9abe978a39b5c97f4704538dc30d3e24b9
snoopy1224 <misty12024@gmail.com>
2023-08-12 12:47:35 +0900

    댓글기능 (useEffect)

--- forum/app/detail/[id]/Comment.js
+++ forum/app/detail/[id]/Comment.js
@@ -1,4 +1,5 @@
 "use client";
+// 서버에게 DB출력결과 달라고 부탁은 가능
 import { useState } from "react";
 
 export default function Comment(props) {
@@ -14,7 +15,10 @@ export default function Comment(props) {
       <button
         onClick={() => {
           console.log(comment);
-          fetch("/URL", { method: "POST", body: comment });
+          fetch("/api/comment/new", {
+            method: "POST",
+            body: JSON.stringify({ comment: comment, _id: props._id }),
+          });
         }}
       >
         댓글전송
