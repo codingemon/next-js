9b780ac16b8bce49c9810a26e21f8a84814b4098
snoopy1224 <misty12024@gmail.com>
2023-08-13 19:49:04 +0900

    댓글

--- forum/app/detail/[id]/Comment.js
+++ forum/app/detail/[id]/Comment.js
@@ -4,18 +4,28 @@ import { useEffect, useState } from "react";
 
 export default function Comment(props) {
   let [comment, setComment] = useState("");
-
+  let [data, setData] = useState([]);
   // 쓸데없는 코드 보관함 (ajax, 타이머등 넣어줌)
   // useEffect 특징
   // 1. html 로드/재렌더링될 때 마다 실행됨
   // 2. html 보여준 후에 늦게 실행시작
   useEffect(() => {
-    fetch();
+    fetch("/api/comment/list?id=" + props._id)
+      .then((r) => r.json())
+      .then((result) => {
+        setData(result);
+      });
   }, []);
 
   return (
     <div>
       <div>댓글목록</div>
+      <hr></hr>
+      {data.length > 0
+        ? data.map((a, i) => {
+            <p key={i}>{a.content}</p>;
+          })
+        : "댓글없음"}
       <input
         onChange={(e) => {
           setComment(e.target.value);
