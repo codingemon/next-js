fa1f37c964647c687c4dcce7f5f0cc626b358359
snoopy1224 <misty12024@gmail.com>
2023-08-14 20:05:32 +0900

    이미지 업로드 (aws s3 업로드 x)

--- forum/app/write/page.js
+++ forum/app/write/page.js
@@ -1,10 +1,37 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Write() {
+  let [src, setSrc] = useState("");
+
   return (
     <div className="p-20">
       <h4>글작성</h4>
       <form action="/api/post/new" method="POST">
         <input name="title" placeholder="글제목" />
         <input name="content" placeholder="글내용" />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={async (e) => {
+            let file = e.target.files[0];
+            let filename = encodeURIComponent(file.name);
+            let res = await fetch("/api/post/image?file=file.name" + filename);
+            res = await res.json();
+
+            const formData = new FormData();
+            Object.entries({ ...res.fields, file }).forEach(([key, value]) => {
+              formData.append(key, value);
+            });
+            let 업로드결과 = await fetch(res.url, {
+              method: "POST",
+              body: formData,
+            });
+            setSrc(업로드결과.url + "/" + filename);
+          }}
+        />
+        <img src="{src}" />
         <button type="sumbit">작성완료</button>
       </form>
     </div>
